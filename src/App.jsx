324c18862b4bc582eb770cf0784f2dae400fce22
Vinicios Neves <vinicios.neves@mac>
2025-06-02 21:22:42 +0100

    video 2.5

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import { SubHeading } from "./components/SubHeading"
 import { ToDoItem } from "./components/ToDoItem"
 import { ToDoList } from "./components/ToDoList"
 import { TextInput } from "./components/TextInput"
+import { Button } from "./components/Button"
+import { TodoForm } from "./components/TodoForm"
 
 const todos = [
   {
@@ -62,6 +64,10 @@ function App() {
     console.log('alternar modal')
   }
 
+  const addTodo = () => {
+    console.log('precisamos add um novo todo')
+  }
+
   return (
     <main>
       <Container>
@@ -85,10 +91,8 @@ function App() {
           </ToDoList>
           <Footer>
             <Dialog isOpen={showDialog} onClose={toggleDialog}>
-              <form>
-                <TextInput placeholder="Digite o item que deseja adicionar" />
-              </form>
-            </Dialog>
+              <TodoForm onSubmit={addTodo} />
+            </Dialog> 
             <FabButton onClick={toggleDialog} >
               <IconPlus />
             </FabButton>
