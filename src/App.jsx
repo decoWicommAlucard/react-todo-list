54892d16f6879f6639405bda20466167c6423941
Vinicios Neves <vinicios.neves@mac>
2025-06-04 22:07:18 +0100

    video 5.3

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,15 @@ import { TodoGroup } from "./components/TodoGroup"
 
 function App() {
 
-  const { todos, addTodo, showDialog, openFormTodoDialog, closeFormTodoDialog, selectedTodo } = use(TodoContext)
+  const { todos, addTodo, showDialog, openFormTodoDialog, closeFormTodoDialog, selectedTodo, editTodo } = use(TodoContext)
   
   const handleFormSubmit = (formData) => {
-    addTodo(formData)
-    openFormTodoDialog()
+    if (selectedTodo) {
+      editTodo(formData)
+    } else {
+      addTodo(formData)
+    }
+    closeFormTodoDialog()
   }
 
   return (
@@ -44,7 +48,7 @@ function App() {
                 defaultValue={selectedTodo?.description}  
               />
             </Dialog>
-            <FabButton onClick={openFormTodoDialog} >
+            <FabButton onClick={() => openFormTodoDialog()} >
               <IconPlus />
             </FabButton>
           </Footer>
--- src/components/TodoProvider/index.jsx
+++ src/components/TodoProvider/index.jsx
@@ -54,6 +54,20 @@ export function TodoProvider({ children }) {
         })
     }
 
+    const editTodo = (formData) => {
+        setTodos(prevState => {
+            return prevState.map(t => {
+                if (t.id == selectedTodo.id) {
+                    return {
+                        ...t,
+                        description: formData.get('description')
+                    }
+                }
+                return t
+            })
+        })
+    }
+
     const deleteTodo = (todo) => {
         setTodos(prevState => {
             return prevState.filter(t => t.id != todo.id)
@@ -69,7 +83,8 @@ export function TodoProvider({ children }) {
                 showDialog,
                 openFormTodoDialog,
                 closeFormTodoDialog,
-                selectedTodo
+                selectedTodo,
+                editTodo
             }}
         >
             {children}
