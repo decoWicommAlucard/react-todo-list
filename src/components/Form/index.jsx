c511e01cede5862e86a782db6d61edf7181f358f
Marcus Vinicius Nascimento Ferreira <198228252+decoWicommAlucard@users.noreply.github.com>
2025-11-20 17:41:09 -0300

    feat: enhance Form component with controlled input and focus management using useRef

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,18 +1,48 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 
 export default function Form() {
   const labelRef = useRef(null)
   const inputRef = useRef(null)
 
+  useEffect(() => {
+    labelRef.current.innerHTML = 'Nome do usuário'
+    labelRef.current.style.color = 'blue'
+
+    inputRef.current.focus()
+  }, [])
+
+  const enviarFormulario = (event) => {
+    event.preventDefault()
+    alert(`Nome enviado: ${inputRef.current.value}`)
+  }
+
   return (
-    <form>
+    <form onSubmit={enviarFormulario}>
       <div>
         <label ref={labelRef} htmlFor="nome">
           Nome
         </label>
       </div>
       <div>
-        <input type="text" id="nome" name="nome" ref={inputRef} />
+        <input
+          type="text"
+          id="nome"
+          name="nome"
+          ref={inputRef}
+          placeholder="Digite seu nome..."
+        />
+      </div>
+
+      <div>
+        <label htmlFor="email">Email</label>
+      </div>
+      <div>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Digite seu email..."
+        />
       </div>
       <button>Salvar</button>
     </form>
